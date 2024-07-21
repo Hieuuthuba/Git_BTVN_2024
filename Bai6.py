370ce6daddb4921134c39363d9a89d2e12bdf5bc
Hiuu <chiheo1512@gmail.com>
2024-07-21 11:09:43 +0700

    Delete Bai6.py

--- Bai6.py
+++
@@ -1,9 +0,0 @@
-n = int(input('nhập n ='))
-for i in range(1,n+1):
-    tong = 0
-    for a in range(1,i):
-        if i % a == 0:
-            tong = tong + a
-    if tong == i:
-        print(i)
-
